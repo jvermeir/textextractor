a36a9fe8b07108c45c6967a069518d8c387dfcc9
Jan Vermeir <jvermeir@hotmail.com>
2022-04-13 11:09:09 +0200

    print commands to convert to epub and add to db

--- src/main/java/nl/vermeir/extractor/TextExtractor.java
+++ src/main/java/nl/vermeir/extractor/TextExtractor.java
@@ -1,9 +1,14 @@
 package nl.vermeir.extractor;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public abstract class TextExtractor {
     public abstract TextExtractor removeHeader();
@@ -55,5 +60,26 @@ public abstract class TextExtractor {
                 .removeSecondHeader()
                 .removeEndOfFooter();
         text.writeToFile();
+
+        poorMansCalibreSolution(text);
+    }
+
+    private static void poorMansCalibreSolution(TextExtractor text) {
+        // Note: calling calibredb commands using Java's Process class doesn't seem to work.
+        // When called through Process, calibredb can't find files.
+
+        String txtFileName = text.story.title + " - " + text.story.author + ".txt";
+        txtFileName = txtFileName.replaceAll(" ", "\\\\ ");
+
+        String calibreConvert = "/Applications/calibre.app/Contents/MacOS/ebook-convert";
+        String epubName = txtFileName.replace(".txt", ".epub");
+        String[] convertCommand = {calibreConvert, txtFileName,epubName };
+        System.out.println(String.join(" ", convertCommand));
+
+        String calibredb = "/Applications/calibre.app/Contents/MacOS/calibredb";
+        String author = "\"" + text.story.author + "\"";
+        String title = "\"" + text.story.title + "\"";
+        String[] addCommand = {calibredb, "add", "-a", author, "-t", title, epubName};
+        System.out.println(String.join(" ", addCommand));
     }
 }
